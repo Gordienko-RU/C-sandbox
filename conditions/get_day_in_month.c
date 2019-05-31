e9df8a0b52ac699ff249f102c6ee45ad5b0c0ee8
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2019-05-31 17:40:43 +0300

    solve condition task about getting days count depends on month

--- conditions/get_day_in_month.c
+++ conditions/get_day_in_month.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
+
+int getDaysCount(int, int);
 
 int main() {
-  int monthNumber;
+  int monthNumber, dayCount;
   time_t s = time(NULL);
   struct tm *currentTime = localtime(&s);
+  const int currentYear = currentTime->tm_year + 1900;
 
-  printf("%d", (currentTime->tm_year));
-  printf("insert month index");
+  printf("insert month index \n");
   scanf("%d", &monthNumber);
 
-  // if (monthNumber == 2 && currentTime->tm_yday) {
-  //   printf("case");
-  // }
+  printf("\n day count - %d", getDaysCount(monthNumber, currentYear));
 
   return 0;
+}
+
+int getDaysCount(int monthNumber, int currentYear) {
+    switch (monthNumber) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11: 
+      return 30;
+    case 2: {
+      bool isLeap = (currentYear % 100 == 0)
+        ? (currentYear % 400 == 0)
+        : (currentYear % 4 == 0);
+      return isLeap ? 29 : 28;
+    }
+    default: 
+      return 0;
+  }
 }
