44880af89630bbd295ffd44cfbfb3dde655bb9ce
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2020-08-26 15:28:41 +0300

    playing with arrays

---
+++ collections/index.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  const int fullArray[] = { 2, 5, 3, 5 };
+
+  // NOTE: will output address of first element
+  cout << "full array: " << fullArray << endl;
+  cout << "size of full array: " << sizeof(fullArray) << endl;
+
+  int arrayWithGaps[4];
+
+  arrayWithGaps[0] = 2;
+  arrayWithGaps[2] = 3;
+  arrayWithGaps[3] = 5;
+
+  cout << "garbage value: " << arrayWithGaps[1] << endl;
+  cout << "size of full array: " << sizeof(fullArray) << endl;
+
+  // NOTE: initialize array with values regardless on scope
+  int arrayOfZeros[10] = { 0 };
+
+  for(int i = 0; i < 10; i++) {
+    cout << "element value: " << arrayOfZeros[i] << endl;
+  };
+
+  cout << "Access not allocated memory: " << arrayOfZeros[10] << endl; 
+
+  return 0;
+};
