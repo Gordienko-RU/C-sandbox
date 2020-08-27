e856fa3ee11683da0fe9aedfd7d9cf7a5a49f27b
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2020-08-27 23:41:54 +0300

    playing with multidimensional arrays

---
+++ collections/multiplicationTable.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int multiplicationTable[10][10];
+
+  for(int rowIndex = 0; rowIndex < 10; rowIndex++) {
+    for(int columnIndex = 0; columnIndex < 10; columnIndex++) {
+      const int numberToMultiply = columnIndex + 1;
+      const int multiplier = rowIndex + 1;
+
+      multiplicationTable[rowIndex][columnIndex] = numberToMultiply * multiplier;
+    };
+  };
+
+  cout << "Multiplication table:" << endl;
+
+  // TODO: DRY - move to function or use implemented iterator or array class
+    for(int rowIndex = 0; rowIndex < 10; rowIndex++) {
+      for(int columnIndex = 0; columnIndex < 10; columnIndex++) {
+        cout << " " << multiplicationTable[rowIndex][columnIndex] << " " << flush;
+      };
+
+      cout << endl;
+    };
+
+    return 0;
+}
