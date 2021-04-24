ee0cd32d7dd8207415c36273ec2e3d944ee27a00
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2021-04-24 19:58:33 +0300

    playing with const

---
+++ const keyword/index.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+  // class SomeClass
+  // {
+  // private:
+  //   int val;
+  // public:
+  //   void setVal(int val) {
+  //     this->val = val;
+  //   }
+  //   void getVal() const {
+  //     this->val = 3; // Error.
+  //     cout << this->val << endl;
+  //   };
+  // };
+
+int main() {
+  // /*
+  // const int immutableVariable = 12;
+  // immutableVariable = 14; // Error here, since we are using const.
+
+  // const int* pointerToImmutableVariable = &immutableVariable; // Correct pointer.
+  // *pointerToImmutableVariable = 14; // Error here, can not change since original variable declared using const.
+  
+  // int anotherVariable = 15;
+  // pointerToImmutableVariable = &anotherVariable; // Since pointer is not constant it's ok.
+
+  // const int* pointerToAnotherVariable = &anotherVariable;
+  // *pointerToAnotherVariable = 14; /* Error here, since we declare variable as immutable while
+  // pointer creation */
+
+  // /*
+  // anotherVariable = 17; // But this is ok.
+
+  // int* const anotherPointerToAnotherVariable = &anotherVariable;
+  // *pointerToAnotherVariable = 17;
+  
+  // int newVariable = 12;
+  // pointerToAnotherVariable = &newVariable; // Error here, pointer is constant
+
+  // const int* const imutablePointerToImmutableVariable = &newVariable;
+  // *imutablePointerToImmutableVariable = 19; // Error.
+  // imutablePointerToImmutableVariable = &anotherVariable; // Error.
+  // */
+
+  // SomeClass instance;
+
+  // instance.setVal(145);
+  // instance.getVal();
+
+
+  return 0; 
+}
