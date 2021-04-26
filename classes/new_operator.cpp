6aa03bed6c91ac5730c5d61bc2d7799abcae8913
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2021-04-26 22:24:14 +0300

    return object from function

--- classes/new_operator.cpp
+++ classes/new_operator.cpp
@@ -12,7 +12,38 @@ public:
   ~Test() {
     cout << "Object deleted." << endl;
   };
+  void setName(string name) {
+    this->name = name;
+  };
+  void getName() {
+    cout << this->name << endl;
+  };
 };
+
+// // Unefficient.
+// Test createTest(string name) {
+//   Test test;
+//   test.setName(name);
+
+//   return test; // Object is destroyed since scope closed, so copy of object should be created
+//   // to return it.
+// }
+// ...
+// Test inst = createTest("test 1"); // Copy constructor call.
+
+// That function creates pointer to return address of object,
+// therefor we can avoid temporary copy creation and call of copy constructor
+// while assign it to variable.
+Test* createTest(string name) {
+  Test* test = new Test();
+  test->setName(name);
+
+  return test;
+}
+// ...
+// Test* inst3 = createTest("test 1"); // Address returned which is cheap, and no calls of copy constructor.
+
+// delete inst3; // Is not very clear, since memory allocation and clearance happened in different places.
 
 int main() {
   Test inst1; // Memory will be cleared automatically, after function finished.
@@ -22,6 +53,10 @@ int main() {
 
   delete inst2;
 
+  Test* inst3 = createTest("test 1");
+  inst3->getName();
+
+  delete inst3; 
+
   return 0;
 }
-
