b8eb90759e924a39f24bf376b403195cc0aa6990
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2021-04-29 21:21:29 +0300

    play with array of objects

--- classes/new_operator.cpp
+++ classes/new_operator.cpp
@@ -6,20 +6,17 @@ class Test {
 private:
   string name;
 public:
-  Test() {
-    cout << "Object created." << endl;
-  };
-  ~Test() {
-    cout << "Object deleted." << endl;
-  };
+  Test() {};
+  ~Test() {};
   void setName(string name) {
     this->name = name;
   };
-  void getName() {
+  void getName() const {
     cout << this->name << endl;
   };
 };
 
+// OBJECT RETURNING FROM FUNCTION EXAMPLES:
 // // Unefficient.
 // Test createTest(string name) {
 //   Test test;
@@ -34,29 +31,54 @@ public:
 // That function creates pointer to return address of object,
 // therefor we can avoid temporary copy creation and call of copy constructor
 // while assign it to variable.
-Test* createTest(string name) {
-  Test* test = new Test();
-  test->setName(name);
+// Test* createTest(string name) {
+//   Test* test = new Test();
+//   test->setName(name);
 
-  return test;
-}
+//   return test;
+// }
 // ...
 // Test* inst3 = createTest("test 1"); // Address returned which is cheap, and no calls of copy constructor.
 
 // delete inst3; // Is not very clear, since memory allocation and clearance happened in different places.
 
 int main() {
-  Test inst1; // Memory will be cleared automatically, after function finished.
-  Test* inst2 = new Test(); // Need to delete object explicitly, to clear memory and call destructor.
+  // PLAYING WITH NEW OPERATOR
+  // Test inst1; // Memory will be cleared automatically, after function finished.
+  // Test* inst2 = new Test(); // Need to delete object explicitly, to clear memory and call destructor.
 
   // inst2->name // Arrow here is shorthand for (*inst2).name.
 
-  delete inst2;
+  // delete inst2;
 
-  Test* inst3 = createTest("test 1");
-  inst3->getName();
+  // Test* inst3 = createTest("test 1");
+  // inst3->getName();
 
-  delete inst3; 
+  // delete inst3; 
+
+  // Test* tests = new Test[10]; // Create array of several instances.
+
+  // delete [] tests;  // Culry brackets needed to tell that we want to delete the whole array,
+  // // not only first element.
+
+  // int* integerArrAllocated = new int[100]; // Memory can be allocated with new for primitives as well.
+
+  // delete [] integerArrAllocated;
+
+  // TEST TASK FROM COURSE
+  const int ASCII_INDENT = 65;
+  const int CHARACTERS_IN_ALPABET_COUNT = 26;
+
+  Test tests[CHARACTERS_IN_ALPABET_COUNT];
+
+  for(int i = 0; i < CHARACTERS_IN_ALPABET_COUNT; i++) {
+    const int charCode = i + ASCII_INDENT;
+    const string alphabetCharacter = string(1, charCode);
+
+    Test& testRef = tests[i];
+    testRef.setName(alphabetCharacter);
+    testRef.getName();
+  };
 
   return 0;
 }
