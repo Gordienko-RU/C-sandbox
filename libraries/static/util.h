7bf5bf42fb5009c0f8c7989f9010f3ca524f5fac
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2021-05-29 22:22:14 +0300

    create static library

---
+++ libraries/static/util.h
@@ -0,0 +1,10 @@
+#ifndef UTIL
+#define UTIL
+
+// Object files with definition are hidden into static library archieve.
+namespace util {
+  double sqr(double n);
+  int sqr(int n);
+}
+
+#endif // !1
