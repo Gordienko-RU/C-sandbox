2b758dea655b6524d68c5dc516eaa35d0ae94c9b
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2021-09-30 21:17:10 +0300

    playing with runtime polumorphism

---
+++ language_basis/polymorphism/run_time_method_overriding.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class A {
+  public:
+    void virtual log() = 0; // pure virtual function
+};
+
+class B: public A {
+  public:
+    void log() {
+      cout << "Derived class B" << endl;
+    }
+};
+
+class C: public A {
+  public:
+    void log() {
+      cout << "Derived class C" << endl;
+    }
+};
+
+int main() {
+  B b; C c; B * bP = new B;
+  A* arr[] = { &b, &c, bP };
+
+  for (int i = 0; i < 3; i++) {
+    arr[i]->log();
+  }
+  /*
+    Derived class B
+    Derived class C
+    Derived class B
+  */
+
+
+  return 0;
+}
+
