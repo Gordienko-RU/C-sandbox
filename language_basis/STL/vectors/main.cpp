6dd68765941205860671038ab7dbd99ddfdc0d0b
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2021-10-29 17:47:51 +0300

    vectors capacity handling

--- language_basis/STL/vectors/main.cpp
+++ language_basis/STL/vectors/main.cpp
@@ -16,5 +16,25 @@ int main() {
     cout << "item is: " << *it << endl;
   }
 
+  int capacity = numbers.capacity();
+  cout << "initial capacity is: " << capacity << endl;
+  
+  for(int i = 0; i < 100; i++) {
+    numbers.push_back(0);
+
+    if (numbers.capacity() != capacity) {
+      capacity = numbers.capacity();
+      cout << "capacity is: " << capacity << endl;
+    }
+  }
+  /*
+    capacity is: 4
+    capacity is: 8
+    capacity is: 16
+    capacity is: 32
+    capacity is: 64
+    capacity is: 128
+  */
+
   return 0;
 };
