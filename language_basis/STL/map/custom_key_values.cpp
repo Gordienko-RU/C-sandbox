51431cc32c13d199dbf66b7c87d7c43f856d1eca
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2021-11-22 21:24:25 +0300

    instance as a map key

--- language_basis/STL/map/custom_key_values.cpp
+++ language_basis/STL/map/custom_key_values.cpp
@@ -4,18 +4,47 @@
 using namespace std;
 
 class Custom {
-  private:
-    int val;
-  
   public:
-  Custom(): val(0) {};
-  Custom(int val): val(val) {};
+  int val;
+  int val1;
+
+  Custom(): val(0), val1(0) {};
+  Custom(int val): val(val), val1(0) {};
+  Custom(int val, int val1): val(val), val1(val1) {};
+
+  bool operator<(const Custom &instanceToCompareWith) const {
+    cout << "comparison triggered with" << instanceToCompareWith.val << endl;
+
+    return instanceToCompareWith.val > this->val;
+  }
 };
 
 int main() {
-  map<string, Custom> mapInstance;
+  // class instance as a value
+  map<string, Custom> mapOfInstance;
 
-  mapInstance["first"]; // default constructor present, so that's ok
+  mapOfInstance["first"]; // default constructor present, so that's ok
+
+  // class instance as a key
+  map<Custom, string> mapByInstance;
+
+  mapByInstance[Custom(5, 0)] = "first"; // < operator overloading implemented, so we can use instance as a key
+  mapByInstance[Custom(5, 4)] = "new";
+
+  /*
+    Objects were different, but because of less operator implementation map identifies them as
+    equal, and update only value, not a key.
+  */
+  cout << mapByInstance.find(5)->first.val1 << endl; // 0 - original key value
+  cout << mapByInstance.find(5)->second << endl; // "new"
+
+  /*
+    Class wrap passed value with matched constructor, and than, using custom
+    comparisson operator, define are instances equal, or not.
+  */
+  if (mapByInstance.find(5) != mapByInstance.end()) {
+    cout << "found by instance member" << endl; // logged
+  }
 
   return 0;
 }
