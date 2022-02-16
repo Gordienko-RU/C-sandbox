21465087e556d66c79abada11f30440fe6403669
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2022-02-16 18:02:24 +0300

    playing with variable capturing

--- language_basis/lambda expressions/main.cpp
+++ language_basis/lambda expressions/main.cpp
@@ -8,6 +8,13 @@ void log(const int &val) {
   cout << val << endl;
 }
 
+template<typename A>
+auto sumAndLogFactory(A baseValue) {
+  return [baseValue](A value) {
+    cout << baseValue + value << endl;
+  };
+}
+
 int main() {
   vector<int> numbers {1, 2, 3, 4};
 
@@ -15,6 +22,18 @@ int main() {
   for_each(numbers.begin(), numbers.end(), log);
   cout << endl;
   for_each(numbers.begin(), numbers.end(), [](const int &val) -> void { cout << val << endl; } );
+  
+  // capture clause
+  cout << endl;
+  int a = 23;
+
+  [&a]() {
+    cout << a << endl;
+  }();
+
+  // function creation
+  auto f = sumAndLogFactory(12);
+  f(5); // 17
 
   return 0;
 }
