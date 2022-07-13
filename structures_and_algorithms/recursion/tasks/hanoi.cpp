96d59a8d2e5cce817b4a740f722060b85b477d3d
Roman <roman.gordienko@instinctools.com>
2022-07-13 19:03:42 +0300

    solve hanoi problem

--- structures_and_algorithms/recursion/tasks/hanoi.cpp
+++ structures_and_algorithms/recursion/tasks/hanoi.cpp
@@ -3,23 +3,54 @@
 
 using namespace std;
 
+// Time complexity - 3^n
+void fun(
+  int amountOfDisksToMove, 
+  vector<int>& sourceTower, 
+  vector<int>& targetTower, 
+  vector<int>& tempTower
+) {
+  switch(amountOfDisksToMove) {
+    case 0: {
+      return;
+    }
 
-void fun(vector<int>& a, vector<int>& b, vector<int>& c) {
-  const int sizeOfA = a.size();
-  const int sizeOfB = b.size();
-  const int sizeOfC = c.size();
+    case 1: {
+      const int elementToBeMoved = sourceTower.back();
+      targetTower.push_back(elementToBeMoved);
+      sourceTower.pop_back();
 
-  const bool allDisksMovedToTargetTower = sizeOfA == 0 && sizeOfB == 0;
+      return;
+    }
 
-  if (allDisksMovedToTargetTower) {
-    return;
+    case 2: {
+      fun(1, sourceTower, tempTower, targetTower);
+      fun(1, sourceTower, targetTower, tempTower);
+      fun(1, tempTower, targetTower, sourceTower);
+
+      return;
+    }
+
+    default: {
+      fun(amountOfDisksToMove - 1, sourceTower, tempTower, targetTower);
+      fun(1, sourceTower, targetTower, tempTower);
+      fun(amountOfDisksToMove - 1, tempTower, targetTower, sourceTower);
+
+      return;
+    }
   }
-
-  
 }
 
 int main() {
-  // cout << fun(7) << endl;
+  vector<int> a = { 1, 2, 3, 4, 5 };
+  vector<int> b;
+  vector<int> c;
+
+  fun(5, a, c, b);
+
+  cout << "a size " << a.size() << endl;
+  cout << "b size " << b.size() << endl;
+  cout << "c size " << c.size() << endl;
 
   return 0;
 }
